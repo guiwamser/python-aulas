60865c0b2a9cf7c73d507f7c305d6e3bf318f1c2
Guilherme L. Wamser <guilherme.wamser@proway.treina>
2022-10-11 21:36:42 -0300

    ingles

---
+++ E-commerce/id_auto.sql
@@ -0,0 +1,9 @@
+alter table cliente alter id_cliente add generated always as identity
+
+alter table funcionarios alter id_funcionario add generated always as identity
+
+alter table compra  alter id_compra add generated always as identity
+
+alter table filial alter id_filial add generated always as identity
+
+alter table item alter id_item add generated always as identity
