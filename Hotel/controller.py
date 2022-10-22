6b5523142f8b3ffb453370da7ec8df5cd0142919
Guilherme L. Wamser <guilherme.wamser@proway.treina>
2022-10-21 21:43:10 -0300

    UP

--- Hotel/controller.py
+++ Hotel/controller.py
@@ -1,9 +1,4 @@
-def checkin():
-    hospede = {}
-    hospede['Nome'] = str(input('Seu nome: '))
-    hospede['sobreNome'] = str(input('Seu sobrenome: '))
-    hospede['idade'] = int(input('Sua idade: '))
-    hospede['cpf'] = int(input('Seu cpf: '))
+def checkin(hospede):
     with open('Hotel/hotel.txt', 'a') as arquivo:
         arquivo.write(str(hospede)+'\n')
 
@@ -11,11 +6,10 @@ def relatorio():
     with open('Hotel/hotel.txt', 'r') as arquivo:  
         print(arquivo.read())
 
-def procurar():
-    hospedeFind = str(input("Digite o nome desejado:"))
+def procurar(hospedeFind):
     index=0
     flag=0
-    arquivo = open("hotel/hotel.txt", "r") 
+    arquivo = open("Hotel/hotel.txt", "r") 
     for line in arquivo:
         index +=1
         if hospedeFind == eval(line)['Nome']:
@@ -24,5 +18,36 @@ def procurar():
     if flag == 0:
         print("Hospede não encontrado")
 
-
-
+def checkout(hospedeFind):
+    index=0
+    flag=0
+    arquivo = open("Hotel/hotel.txt", "r") 
+    for line in arquivo:
+        index +=1
+        if hospedeFind == eval(line)['Nome']:
+            chave=index
+            flag = 1
+    if flag == 0:
+        print("Hospede não encontrado")
+    else:
+        try:
+            with open('hotel/hotel.txt', 'r') as fr:
+                # reading line by line
+                lines = fr.readlines()
+                
+          
+                # pointer for position
+                ptr = 1
+      
+                # opening in writing mode
+                with open('hotel/hotel.txt', 'w') as fw:
+                    for line in lines:
+                
+                        # we want to remove 5th line
+                        if ptr != chave:
+                            fw.write(line)
+                        ptr += 1
+            print("Deleted")
+      
+        except:
+            print("Oops! someting error")
--- Hotel/mainHotel.py
+++ Hotel/mainHotel.py
@@ -1,46 +1,51 @@
 import datetime
 from time import sleep
-from controller import checkin
-from controller import relatorio
-from controller import procurar
+from controller import checkin, relatorio, procurar, checkout
+
 
 now = datetime.datetime.now()
 hour = now.hour
+def menu():
+    if hour < 12:
+        saudacao = "Bom dia, flor do dia"
+    elif hour < 18:
+        saudacao = "Boa tarde, forasteiro"
+    else:
+        saudacao = "Boa noite, meu consagrado"
 
-if hour < 12:
-    saudacao = "Bom dia, flor do dia"
-elif hour < 18:
-    saudacao = "Boa tarde, forasteiro"
-else:
-    saudacao = "Boa noite, meu consagrado"
+    print('-'*20, 'HOTEL', '-'*20 )
+    print("\n{}!\n".format(saudacao))
 
-print('-'*20, 'HOTEL', '-'*20 )
-print("\n{}!\n".format(saudacao))
+    opcao = 1
 
-opcao = 1
-
-while opcao:
-    print("1 -> Fazer Check-in")
-    print("2 -> Relatório Hospedes")
-    print("3 -> Procurar Hospedes")
-    print("4 -> Fazer Check-Out")
-    print("5 -> Finalizar Atendimento")
-    
-
-    opcao = int(input("Opção: "))
-    
-    if(opcao==1):
-        checkin()
-    if(opcao==2):
-        relatorio()
-    if(opcao==3):
-        procurar()
-    if(opcao==4):
-        checkout()
-    else:()
-
+    while (opcao!=0):
+        print("1 -> Fazer Check-in")
+        print("2 -> Relatório Hospedes")
+        print("3 -> Procurar Hospedes")
+        print("4 -> Fazer Check-Out")
+        print("5 -> Finalizar Atendimento")
+        
+        opcao = int(input("Opção: "))
+        
+        match opcao:
+            case 1:
+                hospede = {}
+                hospede['Nome'] = str(input('Seu nome: '))
+                hospede['sobreNome'] = str(input('Seu sobrenome: '))
+                hospede['idade'] = int(input('Sua idade: '))
+                hospede['cpf'] = int(input('Seu cpf: '))
+                checkin(hospede)
+            case 2:
+                relatorio()
+            case 3:
+                hospedeFind = str(input("Digite o nome desejado: "))
+                procurar(hospedeFind)
+            case 4:
+                hospedeFind = str(input("Digite o nome desejado: "))
+                checkout(hospedeFind)
 
         
+menu()
 
 
 
